2214b1ba36b5e2d89bbd3094d5f41efcfd591e27
Jordan <2601669@sit.singaporetech.edu.sg>
2026-09-11 12:32:39 +0800

    Add continuous stock input loop

--- auditor.py
+++ auditor.py
@@ -1,3 +1,11 @@
 #part 1 initialize inventory and failed entries
 inventory = 0
 failed_entries = 0
+
+#part 2 run loop asking user for stock input until they enter 'quit'
+while True:
+
+    #continuously asking user for stock input
+    stock = input("Enter stock item (or type 'quit' to exit): ")
+
+    
